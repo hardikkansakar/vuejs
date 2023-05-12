09b73224ae25aff176b8d7084b9b83ff521e7274
hardik kansakar <hardik@amarinfotech.com>
2023-05-12 16:25:25 +0530

    call sample dummy api and get all data to displa in order list

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,20 @@
 <template>
-  <div>
-    <!-- <Registration /> -->
-    <!-- <Home msg="Welcome to my App" /> -->
-    <!-- <p></p> -->
-    <!-- <button @click="empFilteredData()">Click Me</button> -->
-  </div>
-  <div>
-    <div v-if="seeMe">
-      <p>i am a first text seen by if</p>
-    </div>
-    <div v-else>
-      <p>i am a default else text</p>
-    </div>
-
-    <button @click="clickIfMe()">Click Me I am if</button>
-  </div>
-  <div>
-    <div v-show="showMeHere">
-      <p>i am a show text</p>
-    </div>
-    <button @click="clickToShowMe()">Click Me i am Show</button>
-  </div>
-  <div>
-    <div v-for="(elm,index) in 10" :key="index">
-      <p>{{ index }} - {{ elm }}</p>
-    </div>
-    
-  </div>
+<product msg="data is here"/>
 </template>
 
 <script>
+import product from './components/ProductApi.vue';
 export default {
-  name: "App",
-  data() {
-    return {
-      // empList:[],
-      emp: [
-        { empName: "Juhi", empID: 12, empSalary: 1000 },
-        { empName: "Mahi", empID: 13, empSalary: 5000 },
-        { empName: "Test", empID: 14, empSalary: 1500 },
-        { empName: "Vaishali", empID: 15, empSalary: 2000 },
-      ],
-      seeMe: false,
-      showMeHere: false,
-    };
+  name:'App',
+  components:{product},
+  data(){
+    return{
+      
+    }
   },
-  methods: {
-    empFilteredData() {
-      const newEmpList = this.emp.filter((elem) => {
-        return elem.empSalary > 1500;
-      });
+  
+  
 
-      console.log("newEmpList", newEmpList);
-    },
-    clickIfMe() {
-      this.seeMe = true;
-    },
-    clickToShowMe() {
-      this.showMeHere = true;
-    },
-  },
+  
 };
 </script>
---
+++ src/components/ProductApi.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Get Product data using API</h1>
+        
+        <!-- <p>{{ products }}</p> -->
+        <button @click="getAllProduct();">Call API</button>
+    </div>
+    <div v-for="elms in products">
+        <ol>
+        <li>ID : {{ elms.id }}</li>
+        <li>Title : {{ elms.title }}</li>
+        <li>Body : {{ elms.body }}</li>
+        </ol>  
+        
+    </div>
+</template>
+<script>
+    export default{
+        name:'ProductApi',
+        components:{},
+        
+        data(){
+            return{
+                products:[],
+                
+            }
+            
+        },
+        methods:{
+            async getAllProduct(){
+                const productData =await fetch('https://jsonplaceholder.typicode.com/posts');
+                this.products = await productData.json();
+                console.log(this.products);
+            }
+        }
+
+    };
+</script>
